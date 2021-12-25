3a6b5cfa416b3d7743fe684c80075528548232e5
oxpan <kipiri@yandex.ru>
2021-12-25 18:37:45 +0700

    добавлен input

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from "react";
 import Button from "./phonebook/UI/button/Button";
+import Input from "./phonebook/UI/input/Input";
 
 function App() {
   return (
     <div className="App">
       {/*<test/>*/}
+        <Input
+            type={"text"}
+            placeholder={"post"}
+        />
+        <Input
+            type={"text"}
+            placeholder={"post"}
+        />
+        <Input
+            type={"text"}
+            placeholder={"post"}
+        />
         <Button>AAAAAA</Button>
     </div>
   );
---
+++ src/phonebook/PhoneBookForms.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PhoneBookForms = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default PhoneBookForms;
