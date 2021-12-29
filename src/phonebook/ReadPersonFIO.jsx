5eb48a2efbb76afc57c2ca7a5a6aa159285014d9
oxpan <kipiri@yandex.ru>
2021-12-29 18:39:52 +0700

    нет запроса на обновление фио

--- src/phonebook/ReadPersonFIO.jsx
+++ src/phonebook/ReadPersonFIO.jsx
@@ -1,17 +1,18 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import Input from "./UI/input/Input";
 import Button from "./UI/button/Button";
 
 const ReadPersonFIO = ({currenID, personList,toChangeFIO}) => {
-    console.log(currenID);
-    console.log(personList[currenID]);
-    console.log(personList[currenID].lastname);
+
     const [readeFIO,setReadFIO] = useState({last:personList[currenID].lastname,
         first:personList[currenID].firstname,
         father:personList[currenID].fathername
     })
 
-    console.log(readeFIO);
+    const readFIOPers = (e) => {
+        e.preventDefault();
+        console.log(readeFIO.last);
+    }
 
     return (
         <div>
@@ -20,19 +21,22 @@ const ReadPersonFIO = ({currenID, personList,toChangeFIO}) => {
                 type={'text'}
                 placeholder={"Фамилия:"}
                 value={readeFIO.last}
+                onChange={e=>setReadFIO({...readeFIO,last: e.target.value})}
                 />
             <Input
                 type={'text'}
                 placeholder={"Имя:"}
                 value={readeFIO.first}
+                onChange={e=>setReadFIO({...readeFIO,first: e.target.value})}
             />
             <Input
                 type={'text'}
                 placeholder={"Отчество:"}
                 value={readeFIO.father}
+                onChange={e=>setReadFIO({...readeFIO,father: e.target.value})}
             />
             <div>
-                <Button>💾</Button>
+                <Button onClick={readFIOPers}>💾</Button>
             </div>
             <hr style={{margin:'15px 0'}}/>
         </div>
